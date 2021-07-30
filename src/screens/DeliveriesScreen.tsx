e86dba51ac2b55c1fa1fe8709d09df7faa6cf943
xesk84 <fj.aparisi@gmail.com>
2021-07-30 15:20:09 +0200

    First push

---
+++ src/screens/DeliveriesScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+type Props = {};
+
+export const DeliveriesScreen = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <Text>Deliveries</Text>
+    </SafeAreaView>
+  );
+};
